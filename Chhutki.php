6d389d3d70e57c7a95c490836c39622b0f12c260
uznabee <54370832+uznabee@users.noreply.github.com>
2019-09-01 13:36:50 +0530

    Add files via upload

---
+++ Chhutki.php
@@ -0,0 +1,80 @@
+<html lang="en">
+	<head>
+	  <title>Caring Cross - Chhutki</title>
+	  <meta charset="utf-8">
+	  <meta name="viewport" content="width=device-width, initial-scale=1">
+	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+	  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+	</head>
+
+	<body>
+		<header>
+		<nav class="navbar navbar-expand-lg navbar-dark fixed-top bg-dark">
+		  	<a class="navbar-brand" href="#"><img src="images/G-R.gif" width="60" height="40">&nbsp;Caring Cross - C<sup>2</sup></a>
+		  	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+		    <span class="navbar-toggler-icon"></span>
+		  	</button>
+		  	<div class="collapse navbar-collapse" id="navbarSupportedContent">
+		    	<ul class="navbar-nav mr-auto">
+			      	<li class="nav-item active">
+			        	<a class="nav-link text-warning" href="index.php">Home <span class="sr-only">(current)</span></a>
+			      	</li>
+			      	<li class="nav-item">
+			        	<a class="nav-link text-warning" href="index.php#Aim">Aim</a>
+			      	</li>
+			      	<li class="nav-item">
+			        	<a class="nav-link text-warning" href="index.php#About">About us</a>
+			      	</li>
+			      	<li class="nav-item">
+			        	<a class="nav-link text-warning" href="index.php#Gallery">Gallery</a>
+			      	</li>
+			      	<li class="nav-item">
+			        	<a class="nav-link text-warning" href="index.php#Contact">Contact</a>
+			      	</li>
+		    	</ul>
+<!-- 		    	<form class="form-inline my-2 my-lg-0">
+		      		<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+		      		<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+		    	</form> -->
+		  	</div>
+		</nav>
+
+ 			<div class="container-fluid" style="margin-top:70px">
+ 				<img src="images/CC-Banner7.jpg" width="800" height="100">
+  			</div>
+		
+		</header>
+
+
+		<section>
+			<div class="container">
+				<h1 class="text-center pt-5"><img src="images/D-L.gif" width="100" height="80">Recovery of Chhutki<img src="images/D-R.gif" width="100" height="80"></h1>
+				<hr class="w-50 mx-auto pt-5">
+				<div class="row">
+					<div class="col-lg-4 col-md-2 col-12 mb-4">
+						<img src="images/CC1.jpg" class="img-fluid">
+					</div>
+					<div class="col-lg-4 col-md-2 col-12 mb-4">
+						<img src="images/CC2.jpg" class="img-fluid">
+					</div>
+					<div class="col-lg-4 col-md-2 col-12 mb-4">
+						<img src="images/CC3.jpg" class="img-fluid">
+					</div>
+					<div class="col-lg-4 col-md-2 col-12 mb-4">
+						<img src="images/CC4.jpg" class="img-fluid">
+					</div>
+					<div class="col-lg-4 col-md-2 col-12 mb-4">
+						<img src="images/CC5.jpg" class="img-fluid">
+					</div>
+					<div class="col-lg-4 col-md-2 col-12 mb-4">
+						<img src="images/CC6.jpg" class="img-fluid">
+					</div>
+				</div>
+			</div>
+		</section>
+
+
+	</body>
+</html>
